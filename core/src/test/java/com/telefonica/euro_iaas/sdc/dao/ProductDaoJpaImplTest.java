d1b8fec69fa7b7880bbfcfddfc6fde62b81f7993
Jesus M Movilla <jesus.movilla@altran.com>
2013-10-02 14:40:25 +0200

    [CLAUDIA-3493-jmms392 UnitaryTests por ProductDao.update with Attribute and Metadata

--- core/src/test/java/com/telefonica/euro_iaas/sdc/dao/ProductDaoJpaImplTest.java
+++ core/src/test/java/com/telefonica/euro_iaas/sdc/dao/ProductDaoJpaImplTest.java
@@ -1,16 +1,14 @@
 /**
- *   (c) Copyright 2013 Telefonica, I+D. Printed in Spain (Europe). All Rights
- *   Reserved.
- * 
- *   The copyright to the software program(s) is property of Telefonica I+D.
- *   The program(s) may be used and or copied only with the express written
- *   consent of Telefonica I+D or in accordance with the terms and conditions
- *   stipulated in the agreement/contract under which the program(s) have
- *   been supplied.
+ * (c) Copyright 2013 Telefonica, I+D. Printed in Spain (Europe). All Rights Reserved. The copyright to the software
+ * program(s) is property of Telefonica I+D. The program(s) may be used and or copied only with the express written
+ * consent of Telefonica I+D or in accordance with the terms and conditions stipulated in the agreement/contract under
+ * which the program(s) have been supplied.
  */
 
 package com.telefonica.euro_iaas.sdc.dao;
 
+import com.telefonica.euro_iaas.sdc.model.Attribute;
+import com.telefonica.euro_iaas.sdc.model.Metadata;
 import com.telefonica.euro_iaas.sdc.model.Product;
 
 /**
@@ -40,11 +38,39 @@ public class ProductDaoJpaImplTest extends AbstractJpaDaoTest {
 
         Product product = new Product();
         product.setName(PRODUCT_NAME);
+        product.addAttribute(new Attribute("key", "value"));
+        product.addMetadata(new Metadata("netkey", "metvalue"));
 
         assertEquals(0, productDao.findAll().size());
         product = productDao.create(product);
         assertEquals(product, productDao.load(PRODUCT_NAME));
         assertEquals(1, productDao.findAll().size());
+
+    }
+
+    /**
+     * Test the update and load method
+     */
+    public void testUpdate() throws Exception {
+        Product product = new Product();
+        product.setName(PRODUCT_NAME);
+        product.addAttribute(new Attribute("key", "value"));
+        product.addMetadata(new Metadata("netkey", "metvalue"));
+
+        assertEquals(0, productDao.findAll().size());
+        product = productDao.create(product);
+
+        Product loadedProduct = productDao.load(PRODUCT_NAME);
+        assertEquals(loadedProduct.getName(), product.getName());
+
+        loadedProduct.addAttribute(new Attribute("key2", "value2"));
+
+        Product updatedProduct = productDao.update(loadedProduct);
+        assertEquals(updatedProduct.getAttributes().size(), 2);
+
+        loadedProduct.addMetadata(new Metadata("met_key2", "met_value2"));
+        updatedProduct = productDao.update(loadedProduct);
+        assertEquals(updatedProduct.getMetadatas().size(), 2);
 
     }
 
