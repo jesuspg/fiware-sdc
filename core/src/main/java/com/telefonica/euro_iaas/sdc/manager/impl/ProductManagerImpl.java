ea843b5082604f55ee0d36ab8c659aa9c4f323b6
Jesus M Movilla <jesus.movilla@altran.com>
2013-10-03 11:27:50 +0200

    [CLAUDIA-3493-jmms392] More CheckStyle Changes

--- core/src/main/java/com/telefonica/euro_iaas/sdc/manager/impl/ProductManagerImpl.java
+++ core/src/main/java/com/telefonica/euro_iaas/sdc/manager/impl/ProductManagerImpl.java
@@ -1,8 +1,12 @@
 /**
- * (c) Copyright 2013 Telefonica, I+D. Printed in Spain (Europe). All Rights Reserved. The copyright to the software
- * program(s) is property of Telefonica I+D. The program(s) may be used and or copied only with the express written
- * consent of Telefonica I+D or in accordance with the terms and conditions stipulated in the agreement/contract under
- * which the program(s) have been supplied.
+ *   (c) Copyright 2013 Telefonica, I+D. Printed in Spain (Europe). All Rights
+ *   Reserved.
+ * 
+ *   The copyright to the software program(s) is property of Telefonica I+D.
+ *   The program(s) may be used and or copied only with the express written
+ *   consent of Telefonica I+D or in accordance with the terms and conditions
+ *   stipulated in the agreement/contract under which the program(s) have
+ *   been supplied.
  */
 
 package com.telefonica.euro_iaas.sdc.manager.impl;
@@ -128,7 +132,7 @@ public class ProductManagerImpl extends BaseInstallableManager implements Produc
     public ProductRelease insert(ProductRelease productRelease) throws AlreadyExistsProductReleaseException,
         InvalidProductReleaseException {
         LOGGER.info("Inserting productRelease " + productRelease.getVersion() + " "
-                        + productRelease.getProduct().getName());
+            + productRelease.getProduct().getName());
         ProductRelease productReleaseOut = insertProductReleaseBBDD(productRelease);
 
         ReleaseDto releaseDto = new ReleaseDto();
