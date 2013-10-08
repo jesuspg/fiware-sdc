89df6d357250e74370ba0d0647e8194769bfa96a
Jesus Perez <jesuspg@tid.es>
2013-10-08 09:55:53 +0200

    add more tests

--- integration-test/src/test/java/com/telefonica/euro_iaas/sdc/ProductIT.java
+++ integration-test/src/test/java/com/telefonica/euro_iaas/sdc/ProductIT.java
@@ -1,31 +1,49 @@
 package com.telefonica.euro_iaas.sdc;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.telefonica.euro_iaas.sdc.client.SDCClient;
 import com.telefonica.euro_iaas.sdc.client.exception.ResourceNotFoundException;
 import com.telefonica.euro_iaas.sdc.client.services.ProductService;
+import com.telefonica.euro_iaas.sdc.model.Attribute;
+import com.telefonica.euro_iaas.sdc.model.Metadata;
+import com.telefonica.euro_iaas.sdc.model.OS;
+import com.telefonica.euro_iaas.sdc.model.Product;
 import com.telefonica.euro_iaas.sdc.model.ProductRelease;
+import com.telefonica.euro_iaas.sdc.model.dto.ProductReleaseDto;
 
 public class ProductIT {
+
+    SDCClient client;
+    String baseUrl;
+    String mediaType;
+
+    @Before
+    public void setUp() {
+        client = new SDCClient();
+        baseUrl = "http://localhost:8080/sdc/rest";
+        mediaType = "application/xml";
+    }
 
     @Test
     public void shouldFailWhenLoadAnUnknownProduct() {
         // given
-
-        SDCClient client = new SDCClient();
-        String baseUrl = "http://localhost:8080/sdc/rest";
-        String mediaType = "application/xml";
+        ProductService productService = client.getProductService(baseUrl, mediaType);
 
         // when
 
-        ProductService productService = client.getProductService(baseUrl, mediaType);
         ProductRelease productRelease = null;
         try {
             productRelease = productService.load("kk", "");
@@ -43,13 +61,9 @@ public class ProductIT {
     public void shouldLoadAProduct() throws ResourceNotFoundException {
         // given
 
-        SDCClient client = new SDCClient();
-        String baseUrl = "http://localhost:8080/sdc/rest";
-        String mediaType = "application/xml";
-
+        ProductService productService = client.getProductService(baseUrl, mediaType);
         // when
 
-        ProductService productService = client.getProductService(baseUrl, mediaType);
         ProductRelease productRelease = productService.load("tomcat", "6");
         // then
         assertNotNull(productService);
@@ -60,17 +74,82 @@ public class ProductIT {
     @Test
     public void shouldListProductCatalog() {
         // given
-        SDCClient client = new SDCClient();
-        String baseUrl = "http://localhost:8080/sdc/rest";
-        String mediaType = "application/xml";
+        ProductService productService = client.getProductService(baseUrl, mediaType);
 
         // when
 
-        ProductService productService = client.getProductService(baseUrl, mediaType);
         List<ProductRelease> list = productService.findAll(null, null, null, null, null, null);
 
         // then
         assertNotNull(list);
+        assertFalse(list.isEmpty());
 
+    }
+
+    @Test
+    public void shouldListProductCatalogFindByTomcat() {
+        // given
+
+        // when
+        ProductService productService = client.getProductService(baseUrl, mediaType);
+
+        List<ProductRelease> list = productService.findAll(null, null, null, null, "tomcat", null);
+        // then
+        assertNotNull(list);
+        assertFalse(list.isEmpty());
+
+    }
+
+    @Test
+    @Ignore
+    public void shouldReturnError404WithFindByUnknownProduct() {
+        // given
+
+        ProductService productService = client.getProductService(baseUrl, mediaType);
+        // when
+
+        List<ProductRelease> list = productService.findAll(null, null, null, null, "kk", null);
+
+        // then
+        assertNotNull(list);
+        assertTrue(list.isEmpty());
+
+    }
+
+    @Test
+    @Ignore
+    public void shouldAddProductToCatalog() {
+        // given
+        String productName = "tomcat";
+        String version = "6";
+        String description = "tomcat 6";
+        InputStream cookbook = this.getClass().getResourceAsStream("/files/" + productName + version + "-cookbook.tar");
+        InputStream files = this.getClass().getResourceAsStream("/files/" + productName + version + "-bin.tar");
+
+        List<OS> supportedOS = new ArrayList<OS>();
+        OS os1 = new OS();
+        os1.setName("Debian");
+        os1.setOsType("95");
+        os1.setVersion("9");
+        supportedOS.add(os1);
+        supportedOS.add(os1);
+        List<ProductRelease> transitableReleases = new ArrayList<ProductRelease>();
+        ProductRelease productRelease1 = new ProductRelease();
+        Product product = new Product();
+        product.setName("tomcat");
+        productRelease1.setProduct(product);
+        transitableReleases.add(productRelease1);
+        List<Attribute> attributes = new ArrayList<Attribute>();
+
+        String releaseNotes = "release notes";
+        List<Metadata> metadatas = new ArrayList<Metadata>();
+        ProductReleaseDto productReleaseDto = new ProductReleaseDto(productName, description, version, releaseNotes,
+                attributes, metadatas, supportedOS, transitableReleases);
+        // when
+        ProductRelease productRelease = client.getProductService(baseUrl, mediaType).add(productReleaseDto, cookbook,
+                files);
+
+        // then
+        assertNotNull(productRelease);
     }
 }
