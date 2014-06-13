9f36dcb46972535c4c0693f664356bd3df47970a
Toni Robres <arobres@tid.es>
2014-06-13 09:35:36 +0200

    add methods to required to install and get information about the installation

--- test/acceptance/commons/utils.py
+++ test/acceptance/commons/utils.py
@@ -3,8 +3,12 @@ __author__ = 'arobres'
 import xmldict
 import string
 import random
+import time
+from commons.rest_utils import RestUtils
 from constants import AUTH_TOKEN_HEADER, TENANT_ID_HEADER, CONTENT_TYPE, CONTENT_TYPE_XML, ACCEPT_HEADER, \
-    CONTENT_TYPE_JSON
+    CONTENT_TYPE_JSON, VDC, TASK_URL,STATUS_XML
+from lxml import etree
+from commons.fabric_utils import execute_file_exist
 
 
 def dict_to_xml(dict_to_convert):
@@ -56,3 +60,80 @@ def delete_keys_from_dict(dict_del, key):
             delete_keys_from_dict(v, key)
 
     return dict_del
+
+
+def delete_keys_when_value_is_none(dict_del):
+
+    default_dict = dict_del.copy()
+    for v in default_dict.keys():
+        if default_dict[v] is None:
+            del dict_del[v]
+    return dict_del
+
+
+def wait_for_task_finished(seconds=30, url=None, status_to_be_finished=None, headers=None):
+
+    headers[ACCEPT_HEADER] = CONTENT_TYPE_JSON
+    for count in range(seconds):
+        request = RestUtils.call_url_task(method='get', url=url, headers=headers)
+        response = request.json()
+        print 'TIME: {}\n STATUS: {}'.format(count, response['@status'])
+
+        if response['@status'] == status_to_be_finished:
+            return True
+        time.sleep(1)
+
+    return False
+
+
+def wait_for_software_installed(seconds=90, status_to_be_finished=True, file_name=None):
+
+    for count in range(seconds/3):
+
+        response = execute_file_exist(test_file_name=file_name)
+        print "FILE EXIST: {}".format(response)
+        if status_to_be_finished:
+            return True
+
+        time.sleep(3)
+    return False
+
+
+def convert_response_to_json(response):
+
+    response_headers = response.headers
+    if response_headers[CONTENT_TYPE] == CONTENT_TYPE_JSON:
+        try:
+            response_body = response.json()
+        except Exception, e:
+            print str(e)
+
+    else:
+        response_body = xml_to_dict(response.content)
+
+    return response_body
+
+
+def get_installation_response(response):
+
+    response_headers = response.headers
+    if response_headers[CONTENT_TYPE] == CONTENT_TYPE_JSON:
+        try:
+            response_body = response.json()
+            status = response_body[STATUS_XML]
+            href = response_body[TASK_URL]
+            vdc = response_body[VDC]
+        except Exception, e:
+            print str(e)
+
+    else:
+        try:
+            response_xml = etree.XML(response.content)
+            href = response_xml.xpath("//task/@href")[0]
+            status = response_xml.xpath("//task/@status")[0]
+            vdc = response_xml.xpath("//task/vdc")[0].text
+
+        except Exception, e:
+            print str(e)
+
+    return href, status, vdc
