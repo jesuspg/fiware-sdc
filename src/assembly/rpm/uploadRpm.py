a821c26c3edb436152d415372b31184894fb65df
Jesus M Movilla <jesus.movilla@altran.com>
2014-09-24 12:27:01 +0200

    Fixing keystone url

--- src/assembly/rpm/uploadRpm.py
+++ src/assembly/rpm/uploadRpm.py
@@ -2,13 +2,13 @@ __author__ = 'jmmovilla'
 
 import requests
 import json
-#import ujson
 
 #AUTHENTICATION CONSTANTS
 AUTH = u'auth'
 TENANT_NAME = u'tenantName'
 USERNAME = u'username'
 PASSWORD = u'password'
+PASSWORD_CREDENTIALS = u'passwordCredentials'
 ACCESS = u'access'
 TOKEN = u'token'
 TENANT = u'tenant'
@@ -16,18 +16,18 @@ ID = u'id'
 
 #AUTHENTICATION
 AUTHENTICATION_HEADERS = {'content-type': 'application/json', 'Accept': 'application/json'}
-TENANT_NAME_VALUE = '00000000000000000000000000000192'
-USERNAME_VALUE = 'arobres@tid.es'
-PWD_VALUE = 'testing'
-KEYSTONE_URL = 'http://130.206.80.57:4731/v2.0/tokens'
+TENANT_NAME_VALUE = 'thalesgroup'
+USERNAME_VALUE = 'henar'
+PWD_VALUE = 'llela1va'
+KEYSTONE_URL = 'http://130.206.80.61:35357/v2.0/tokens'
+
+#TENANT_NAME_VALUE = '00000000000000000000000000000192'
+#USERNAME_VALUE = 'arobres@tid.es'
+#PWD_VALUE = 'testing'
+#KEYSTONE_URL = 'http://130.206.80.57:4731/v2.0/tokens'
 
 #HEADERS
-CONTENT_TYPE = u'content-type'
-CONTENT_TYPE_JSON = u'application/json'
-CONTENT_TYPE_XML = u'application/xml'
 AUTH_TOKEN_HEADER = u'X-Auth-Token'
-TENANT_ID_HEADER = u'Tenant-Id'
-ACCEPT_HEADER = u'Accept'
 
 #REPOSITORY LOCATION
 REPOSITORY_URL = 'http://repositories.testbed.fi-ware.eu:8889/upload'
@@ -35,9 +35,8 @@ REPOSITORY_URL = 'http://repositories.testbed.fi-ware.eu:8889/upload'
 RPM_LOCATION = 'prueba.txt'
 
 def get_token():
-    body = {AUTH: {TENANT_NAME: TENANT_NAME_VALUE, "passwordCredentials": {USERNAME: USERNAME_VALUE,
+    body = {AUTH: {TENANT_NAME: TENANT_NAME_VALUE, PASSWORD_CREDENTIALS: {USERNAME: USERNAME_VALUE,
                                                                            PASSWORD: PWD_VALUE}}}
-    #body = ujson.dumps(body)
     body = json.dumps(body)
     print 'Body to request token: ' + body
     r = requests.request(method='post', url=KEYSTONE_URL, data=body, headers=AUTHENTICATION_HEADERS)
@@ -55,4 +54,5 @@ def upload_rpm():
     r = requests.post(REPOSITORY_URL, files=files)
     print 'Status Code of uploading rpm ' + r.status_code
 
+get_token()
 upload_rpm()
