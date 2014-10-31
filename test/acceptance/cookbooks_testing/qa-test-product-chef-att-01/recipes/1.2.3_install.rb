9b817572248beea8704008f78e4c07d3f60c37bc
Javier Fernandez <jfernandez@tcpsi.es>
2014-10-31 08:47:14 +0100

    REFACTOR indentation

--- test/acceptance/cookbooks_testing/qa-test-product-chef-att-01/recipes/1.2.3_install.rb
+++ test/acceptance/cookbooks_testing/qa-test-product-chef-att-01/recipes/1.2.3_install.rb
@@ -11,8 +11,8 @@ script "install" do
   cwd "/tmp"
   code <<-EOH
   echo "Operation: install; Product: #{node['qa-test-product-chef-att-01']['product_name']}; Version: 1.2.3;" \
-"Att01: #{node['qa-test-product-chef-att-01']['custom_att_01']};"  \
-"Att02: #{node['qa-test-product-chef-att-01']['custom_att_02']}" > #{node['qa-test-product-chef-att-01']['product_name']}_1.2.3_chef
+  "Att01: #{node['qa-test-product-chef-att-01']['custom_att_01']};"  \
+  "Att02: #{node['qa-test-product-chef-att-01']['custom_att_02']}" > #{node['qa-test-product-chef-att-01']['product_name']}_1.2.3_chef
   EOH
 end
 
--- test/acceptance/cookbooks_testing/qa-test-product-chef-att-01/recipes/1.2.3_uninstall.rb
+++ test/acceptance/cookbooks_testing/qa-test-product-chef-att-01/recipes/1.2.3_uninstall.rb
@@ -11,8 +11,8 @@ script "uninstall" do
   cwd "/tmp"
   code <<-EOH
   echo "Operation: uninstall; Product: #{node['qa-test-product-chef-att-01']['product_name']}; Version: 1.2.3;"  \
-"Att01: #{node['qa-test-product-chef-att-01']['custom_att_01']};" \
-"Att02: #{node['qa-test-product-chef-att-01']['custom_att_02']}" >> uninstall_log
+  "Att01: #{node['qa-test-product-chef-att-01']['custom_att_01']};" \
+  "Att02: #{node['qa-test-product-chef-att-01']['custom_att_02']}" >> uninstall_log
   rm #{node['qa-test-product-chef-att-01']['product_name']}_1.2.3_chef
   EOH
 end
