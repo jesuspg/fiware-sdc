a98cb4c3831b2986ef8da13adf783080da7b5203
Jesus Perez <jesuspg@tid.es>
2015-01-12 14:01:40 +0100

    change default image value by empty string refactor code add unit test

--- core/src/test/java/com/telefonica/euro_iaas/sdc/manager/ProductManagerImplTest.java
+++ core/src/test/java/com/telefonica/euro_iaas/sdc/manager/ProductManagerImplTest.java
@@ -24,6 +24,7 @@
 
 package com.telefonica.euro_iaas.sdc.manager;
 
+import static junit.framework.Assert.assertEquals;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyObject;
 import static org.mockito.Mockito.mock;
@@ -32,8 +33,6 @@ import static org.mockito.Mockito.when;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import junit.framework.TestCase;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -51,105 +50,146 @@ import com.telefonica.euro_iaas.sdc.model.searchcriteria.ProductSearchCriteria;
 
 /**
  * Unit test of ProductMangerImpl
+ * 
  * @author jesus.movilla
- *
  */
-public class ProductManagerImplTest extends TestCase {
+public class ProductManagerImplTest {
 
     private Product product, createdProduct = null;
     private ProductDao productDao = null;
     private List<Product> productList;
-    private ProductReleaseManager productReleaseManager=null;
-    private ProductRelease productRelease1, productRelease2,productRelease3=null;
-    private List<ProductRelease> productReleaseList1,productReleaseList2;
+    private ProductReleaseManager productReleaseManager = null;
+    private ProductRelease productRelease1, productRelease2, productRelease3 = null;
+    private List<ProductRelease> productReleaseList1, productReleaseList2;
     private ProductManagerImpl productManager;
     private OS os;
-                    
+
     @Before
     public void setUp() throws Exception {
         productManager = new ProductManagerImpl();
-        
-        product = new Product("productname","productDescription");
+
+        product = new Product("productname", "productDescription");
         List<Metadata> extmetadatas = new ArrayList<Metadata>();
-        extmetadatas.add(new Metadata("image","otro")); 
-        extmetadatas.add(new Metadata("key","value"));
+        extmetadatas.add(new Metadata("image", "otro"));
+        extmetadatas.add(new Metadata("key", "value"));
         product.setMetadatas(extmetadatas);
-        
-        createdProduct = new Product("createdproductname","createdproductDescription");
-        
-        //createdProduct.setMetadatas(extmetadatas);
-        createdProduct.addMetadata(new Metadata("image","otro")); //centos6.3_sdc
-        createdProduct.addMetadata(new Metadata("cookbook_url",""));
-        createdProduct.addMetadata(new Metadata("cloud","yes"));
-        createdProduct.addMetadata(new Metadata("installator","chef"));
-        createdProduct.addMetadata(new Metadata("open_ports","80 22"));
-        createdProduct.addMetadata(new Metadata("key","value"));
-        
+
+        createdProduct = new Product("createdproductname", "createdproductDescription");
+
+        // createdProduct.setMetadatas(extmetadatas);
+        createdProduct.addMetadata(new Metadata("image", "otro")); // centos6.3_sdc
+        createdProduct.addMetadata(new Metadata("cookbook_url", ""));
+        createdProduct.addMetadata(new Metadata("cloud", "yes"));
+        createdProduct.addMetadata(new Metadata("installator", "chef"));
+        createdProduct.addMetadata(new Metadata("open_ports", "80 22"));
+        createdProduct.addMetadata(new Metadata("key", "value"));
+
         productDao = mock(ProductDao.class);
-        when(productDao.create(any(Product.class))).thenReturn(createdProduct); 
-        
-        productList=new ArrayList<Product>();
+        when(productDao.create(any(Product.class))).thenReturn(createdProduct);
+
+        productList = new ArrayList<Product>();
         productList.add(product);
         productList.add(createdProduct);
-        
-        productReleaseManager=mock(ProductReleaseManager.class);
-        
-        productReleaseList1=new ArrayList<ProductRelease>();
-        productReleaseList2=new ArrayList<ProductRelease>();
-        
+
+        productReleaseManager = mock(ProductReleaseManager.class);
+
+        productReleaseList1 = new ArrayList<ProductRelease>();
+        productReleaseList2 = new ArrayList<ProductRelease>();
+
         os = new OS("os1", "1", "os1 description", "v1");
-        
-        productRelease1= new ProductRelease("version1", "releaseNotes1", product, Arrays.asList(os), null);
-        productRelease2= new ProductRelease("version2", "releaseNotes2", product, Arrays.asList(os), null);
-        productRelease3= new ProductRelease("version", "releaseNotes3", createdProduct, Arrays.asList(os), null);
-        
+
+        productRelease1 = new ProductRelease("version1", "releaseNotes1", product, Arrays.asList(os), null);
+        productRelease2 = new ProductRelease("version2", "releaseNotes2", product, Arrays.asList(os), null);
+        productRelease3 = new ProductRelease("version", "releaseNotes3", createdProduct, Arrays.asList(os), null);
+
         productReleaseList1.add(productRelease1);
         productReleaseList1.add(productRelease2);
-        
+
         productReleaseList2.add(productRelease3);
-        
-        
-        
-        
+
     }
-    
+
     /**
      * Test insert Product with metadatas. One is default and the other is new.
+     * 
      * @throws Exception
      */
     @Test
     public void testInsertNewProduct() throws Exception {
         productManager.setProductDao(productDao);
-        
-        when(productDao.load(any(String.class))).thenThrow(new EntityNotFoundException(Product.class, "name", product.getName()));
+
+        when(productDao.load(any(String.class))).thenThrow(
+                new EntityNotFoundException(Product.class, "name", product.getName()));
         Product createdProduct = productManager.insert(product, "");
-        assertEquals(createdProduct.getMetadatas().size(), 6); 
+        assertEquals(createdProduct.getMetadatas().size(), 6);
     }
-    
+
+    @Test
+    public void testInsertNewProductWithNewMetadata() throws Exception {
+
+        // Given
+        Product product = new Product("myproduct", "productDescription");
+        List<Metadata> metadatas = new ArrayList();
+        metadatas.add(new Metadata("key1", "value1"));
+        product.setMetadatas(metadatas);
+        productManager.setProductDao(productDao);
+
+        when(productDao.load(any(String.class))).thenThrow(
+                new EntityNotFoundException(Product.class, "name", product.getName()));
+        when(productDao.create(product)).thenReturn(product);
+        // When
+        Product createdProduct = productManager.insert(product, "0000000000023");
+        // Then
+        assertEquals(7, createdProduct.getMetadatas().size());
+    }
+
+    @Test
+    public void testInsertNewProductWithDefaultMetadata() throws Exception {
+
+        // Given
+        Product product = new Product("myproduct", "productDescription");
+        List<Metadata> metadatas = new ArrayList();
+        metadatas.add(new Metadata("image", "myimage"));
+        metadatas.add(new Metadata("key1", "value1"));
+        product.setMetadatas(metadatas);
+        productManager.setProductDao(productDao);
+
+        when(productDao.load(any(String.class))).thenThrow(
+                new EntityNotFoundException(Product.class, "name", product.getName()));
+        when(productDao.create(product)).thenReturn(product);
+        // When
+        Product createdProduct = productManager.insert(product, "0000000000023");
+        // Then
+        assertEquals(7, createdProduct.getMetadatas().size());
+        assertEquals("myimage", createdProduct.getMetadatas().get(0).getValue());
+    }
+
     /**
      * Test Reinsert old Product.
+     * 
      * @throws Exception
      */
     @Test
     public void testReinsertProduct() throws Exception {
         productManager.setProductDao(productDao);
-        
+
         when(productDao.load(any(String.class))).thenReturn(product);
         Product createdProduct = productManager.insert(product, "");
-        assertEquals(createdProduct.getName(), product.getName()); 
+        assertEquals(createdProduct.getName(), product.getName());
     }
-    
+
     @Test
-    public void testProductAndReleases(){
+    public void testProductAndReleases() {
         productManager.setProductDao(productDao);
         productManager.setProductReleaseManager(productReleaseManager);
-        
-        when(productDao.findByCriteria((ProductSearchCriteria)anyObject())).thenReturn(productList);
-        when(productReleaseManager.findReleasesByCriteria((ProductReleaseSearchCriteria)anyObject())).thenReturn(productReleaseList1).thenReturn(productReleaseList2);
-        
-        ProductSearchCriteria criteria = new ProductSearchCriteria(1,1,"","");
-        List<ProductAndReleaseDto> result=productManager.findProductAndReleaseByCriteria(criteria);
-        
+
+        when(productDao.findByCriteria((ProductSearchCriteria) anyObject())).thenReturn(productList);
+        when(productReleaseManager.findReleasesByCriteria((ProductReleaseSearchCriteria) anyObject())).thenReturn(
+                productReleaseList1).thenReturn(productReleaseList2);
+
+        ProductSearchCriteria criteria = new ProductSearchCriteria(1, 1, "", "");
+        List<ProductAndReleaseDto> result = productManager.findProductAndReleaseByCriteria(criteria);
+
         assertEquals(3, result.size());
     }
 }
